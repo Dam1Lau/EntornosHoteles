61aff2b7fb1eefe7e68ed894537be61e625ec96b
Laura <Laura@Laura-PC>
2018-03-14 19:58:00 +0100

    Moar

--- EntornosAlojamiento/src/entornosalojamiento/Direccion.java
+++ EntornosAlojamiento/src/entornosalojamiento/Direccion.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entornosalojamiento;
 
-/**
- *
- * @author Laura
- */
 public class Direccion {
+    private String calle;
+    private int codPostal;
+    private String provincia;
+    private String comunidad;
+
+    public Direccion() {
+    }
+
+    public Direccion(String calle, int codPostal, String provincia, String comunidad) {
+        this.calle = calle;
+        this.codPostal = codPostal;
+        this.provincia = provincia;
+        this.comunidad = comunidad;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getCodPostal() {
+        return codPostal;
+    }
+
+    public void setCodPostal(int codPostal) {
+        this.codPostal = codPostal;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getComunidad() {
+        return comunidad;
+    }
+
+    public void setComunidad(String comunidad) {
+        this.comunidad = comunidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion: " + "\n Calle: " + calle + "\n Código Postal: " + codPostal + "\nProvincia: " + provincia + "Comunidad: " + comunidad;
+    }
+    
+    
+    
     
 }
--- EntornosAlojamiento/src/entornosalojamiento/Gestor.java
+++ EntornosAlojamiento/src/entornosalojamiento/Gestor.java
@@ -5,10 +5,37 @@
  */
 package entornosalojamiento;
 
-/**
- *
- * @author Laura
- */
+
 public class Gestor {
+    private Alojamiento[] alojamientos;
+
+    public Gestor() {
+    }
+
+    public Alojamiento[] getAlojamientos() {
+        return alojamientos;
+    }
+
+    public void setAlojamientos(Alojamiento[] alojamientos) {
+        this.alojamientos = alojamientos;
+    }
     
+    public void meterAlojamiento(Alojamiento alojamiento){
+        for (int i = 0; i < alojamientos.length; i++) {
+            if(alojamientos[i] == null){
+                alojamientos[i] = alojamiento;
+            }else
+                System.out.println("No se ha podido añadir el alojamiento. No hay espacio.");
+        }
+    
+    }
+    public void consultaAlojamiento(String provincia, double vmedia){
+        for (int i = 0; i < alojamientos.length; i++) {
+            if(alojamientos[i].getDireccion().getProvincia().compareToIgnoreCase(provincia) == 0
+                    && alojamientos[i].getValoracionMedia() >= vmedia){
+                System.out.println(alojamientos[i].toString());
+            }
+        }
+    
+    }
 }
