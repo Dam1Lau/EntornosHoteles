547a0d69de5d9d01ca05a4efe5ca3e776b05bba5
dam1 <dam1@1DAM-04.ies.quevedo>
2018-03-15 14:18:56 +0100

    moar

--- EntornosAlojamiento/src/entornosalojamiento/Gestor.java
+++ EntornosAlojamiento/src/entornosalojamiento/Gestor.java
@@ -5,8 +5,10 @@
  */
 package entornosalojamiento;
 
+import java.util.Scanner;
 
 public class Gestor {
+
     private Alojamiento[] alojamientos;
 
     public Gestor() {
@@ -19,23 +21,65 @@ public class Gestor {
     public void setAlojamientos(Alojamiento[] alojamientos) {
         this.alojamientos = alojamientos;
     }
-    
-    public void meterAlojamiento(Alojamiento alojamiento){
+
+    public void anadirAlojamiento(Alojamiento alojamiento) {
         for (int i = 0; i < alojamientos.length; i++) {
-            if(alojamientos[i] == null){
+            if (alojamientos[i] == null) {
                 alojamientos[i] = alojamiento;
-            }else
+            } else {
                 System.out.println("No se ha podido añadir el alojamiento. No hay espacio.");
+            }
         }
-    
+
     }
-    public void consultaAlojamiento(String provincia, double vmedia){
+
+    public void consultaAlojamientos(String provincia, double vmedia) {
         for (int i = 0; i < alojamientos.length; i++) {
-            if(alojamientos[i].getDireccion().getProvincia().compareToIgnoreCase(provincia) == 0
-                    && alojamientos[i].getValoracionMedia() >= vmedia){
+            if (alojamientos[i].getDireccion().getProvincia().compareToIgnoreCase(provincia) == 0
+                    && alojamientos[i].getValoracionMedia() > vmedia) {
                 System.out.println(alojamientos[i].toString());
             }
         }
-    
+    }
+
+    public void actualizarCategoria(String nombre, int categoria) {
+        for (int i = 0; i < alojamientos.length; i++) {
+            if (alojamientos[i].getNombre().compareToIgnoreCase(nombre) == 0
+                    && alojamientos[i].getClass().getSimpleName().compareTo("Hotel") == 0) {
+                ((Hotel) alojamientos[i]).setCategoria(categoria);
+                i = alojamientos.length;
+            }
+        }
+    }
+
+    public void listarApartamentos(int plazas) {
+        for (int i = 0; i < alojamientos.length; i++) {
+            if (alojamientos[i].getClass().getSimpleName().equalsIgnoreCase("Apartamento")
+                    && ((Apartamento) alojamientos[i]).getNumPlazas() >= plazas) {
+                System.out.println(alojamientos[i]);
+            }
+        }
+    }
+
+    public void eliminarAlojamientos(String provincia) {
+        char respuesta;
+        Scanner lector = new Scanner(System.in);
+        for (int i = 0; i < alojamientos.length; i++) {
+            if (alojamientos[i].getDireccion().getProvincia().equalsIgnoreCase(provincia)) {
+                System.out.println("¿Está seguro de que desea elimiar " + alojamientos[i].getNombre() + " (s/n)");
+                do {                    
+                   respuesta = lector.nextLine().toLowerCase().charAt(0);
+                   if(respuesta == 's'){
+                       alojamientos[i] = null;
+                   }else if (respuesta == 'n'){
+                       System.out.println("");
+                   }
+                    
+                } while (respuesta !='s' && respuesta!='n');
+                
+               
+            }
+        }
+
     }
 }
